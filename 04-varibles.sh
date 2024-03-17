e525427d26dcaf2509157a2e45cee3116f4413db
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-17 18:35:16 +0530

    added family

---
+++ 04-varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Arguments
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, GM"
+echo "$PERSON2: Hi $PERSON1, VGM"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, How are you and what about yor family...."
