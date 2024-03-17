ae7cc91f367950b761539d4937a8335dd5be5223
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-17 21:00:17 +0530

    added family

---
+++ 07-array.sh
@@ -0,0 +1,8 @@
+#!/bib/bash
+
+FRUITS=("APPLE" "BANNANA" "MANGO")
+
+echo "FRUIT Name is ${FRUITS[0]}"
+echo "FRUIT Name is ${FRUITS[1]}"
+echo "FRUIT Name is ${FRUITS[2]}"
+echo "FRUIT Name is ${FRUITS[@]}"
