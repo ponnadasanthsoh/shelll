eaf28e0789f620459bda8f0b00d475eef2f42f02
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-17 20:32:44 +0530

    added family

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "please enter your username:: "
-
 read -s USERNAME # user need to enter user name 
 
-echo "Username is : $USERNAME"
+echo "please enter your Password:: "
+read -s PASSWORD # user need to enter user name
+
+echo "Username is : $USERNAME, Password $PASSWORD"
