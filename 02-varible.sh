330effb826f89f5ed54773962168f9391d69e2fa
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-17 17:21:51 +0530

    added trail

---
+++ 02-varible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## 2nd trial
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, GM"
+echo "$PERSON2: Hi $PERSON1, VGM"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, How are you and "
