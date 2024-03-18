bb8cebf9de197bca63c2bc8b87d6bcfc79ba890c
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-18 20:35:16 +0530

    iam learig

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,6 +5,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "please run this with root user"
+    exit 1
 else 
     echo "You are root user"
-    fi
+fi # fi means reverse of if indicating condtion end 
+
+yum install mysql -y
